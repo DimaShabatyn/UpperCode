b6a66952e71932628fd4587935ad1a0382db8229
Taras Kachmar <kachmar.taras@gmail.com>
2023-01-14 13:07:43 +0200

    Task 3

--- task_3.js
+++ task_3.js
@@ -2,4 +2,11 @@
 // Визначте, в яку чверть години потрапляє
 // це число(у першу, другу, третю чи четверту).
 
-const min = 27;
+const min = 42;
+if(min > 0 && min <= 15){
+    console.log("Перша чверть")
+} else if (min > 15 && min <= 30){
+    console.log("Друга чверть")
+} else if (min > 30 && min <= 45){
+    console.log("Третя чверть")
+} else console.log("Четверта чверть");
