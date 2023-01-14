7b303ade6af2029d5761ff37f2946810f2f209a4
Ivan Mutsko <imucko1996@gmail.com>
2023-01-14 12:49:22 +0200

    1 task

--- task_1.js
+++ task_1.js
@@ -4,3 +4,19 @@
 //Якщо користувач вводить "ECMAScript",
 //то показати через сповіщення: "Вірно!"
 //у протилежному випадку показати:"Не знаєте? ECMAScript!"
+
+//  1
+// const response = prompt("Яке офіційне ім'я JavaScript?");
+// if (response === "ECMAScript") {
+//   alert("Вірно!");
+// } else alert("Не знаєте? ECMAScript!");
+
+//  2
+// const response = prompt("Яке офіційне ім'я JavaScript?");
+
+// response === "ECMAScript" ? alert("Вірно!") : alert("Не знаєте? ECMAScript!");
+
+//  3
+// const response = prompt("Яке офіційне ім'я JavaScript?");
+
+// alert(response === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!");
