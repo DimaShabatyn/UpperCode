b8166a9cb4043aab9e7a51d65fde465ac4fecb72
Ivan Mutsko <imucko1996@gmail.com>
2023-01-28 12:02:30 +0200

    update task 2

--- task_2.js
+++ task_2.js
@@ -4,18 +4,16 @@
 const numbers = [7, 122, 5, 35, 56, 12, 24, 7, 80, 3];
 
 function findSmallerNumber(numbers) {
-    // let minNum = numbers[0];
-    // for (let number of numbers) {
-    //     if (minNum > number) {
-    //       minNum = number;
-    //   }
-    // }
-    // console.log(minNum);
-
+  // let minNum = numbers[0];
+  // for (let number of numbers) {
+  //     if (minNum > number) {
+  //       minNum = number;
+  //   }
+  // }
+  // console.log(minNum);
+  if (Array.isArray(numbers)) {
     console.log(Math.min(...numbers));
-
+  }
 }
 
 findSmallerNumber(numbers);
-
-
