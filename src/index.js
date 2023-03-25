b189b7a8e10c24e4372d5c2fc9146d0aece2ef36
OksanaDrobakha <6249784@gmail.com>
2023-03-25 13:45:40 +0400

    task3

--- src/index.js
+++ src/index.js
@@ -1,38 +1,63 @@
-import './styles/normalize.css';
-import './styles/index.css';
+import "./styles/normalize.css";
+import "./styles/index.css";
 
-import productsApi from './requests/products';
-import usersApi from './requests/users';
-import postsApi from './requests/posts';
-import renderService from './services/markupService';
+import productsApi from "./requests/products";
+import usersApi from "./requests/users";
+import postsApi from "./requests/posts";
+import renderService from "./services/markupService";
 
 // Завдання 1
 const renderProducts = async () => {
-    try {
-       const products = await productsApi.getProducts();
-       renderService.renderProductsMarkup(products);
-    } catch (error) {
-        alert(error);
-    }
-}
+  try {
+    const products = await productsApi.getProducts();
+    renderService.renderProductsMarkup(products);
+  } catch (error) {
+    alert(error);
+  }
+};
 
 // renderProducts();
 
 // Завдання 2
-const form = document.getElementById('singleProductForm');
-const productSection = document.getElementById('singleProduct');
+const form = document.getElementById("singleProductForm");
+const productSection = document.getElementById("singleProduct");
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    productSection.innerHTML = '';
-    const id = e.target.elements.id.value;
-    try {
-        const product = await productsApi.getProductById(id);
-        renderService.renderProductByIdMarkup(product, productSection);
-    } catch (error) {
-        alert(error);
-    }
-})
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  productSection.innerHTML = "";
+  const id = e.target.elements.id.value;
+  try {
+    const product = await productsApi.getProductById(id);
+    renderService.renderProductByIdMarkup(product, productSection);
+  } catch (error) {
+    alert(error);
+  }
+});
+
+//Завдання 3
+const formAddProduct = document.querySelector("#addProductForm");
+
+formAddProduct.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  //   1й спосіб
+  //   const product = {
+  //     title: e.target.elements.title.value,
+  //     description: e.target.elements.description.value,
+  //     price: e.target.elements.price.value,
+  //   };
+
+  //  2й спосіб
+  const productData = new FormData(e.target);
+  const product = {};
+
+  productData.forEach((value, name) => {
+    product[name] = value;
+  });
+  const res = await productsApi.addProduct(product);
+  console.log(res);
+  e.target.reset();
+});
 
 // productsApi.searchProductsByCategory('smartphones');
 // productsApi.filterProducts(5, 10, ['title', 'price']);
